28b3bd23de247a89f42a2b22b46e786c98ec3cdc
phtoselli <phtoselli@gmail.com>
2025-03-24 22:08:12 -0300

    feat: create menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,18 +3,17 @@ import React from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "yellow",
+        tabBarInactiveTintColor: "white",
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
@@ -22,22 +21,46 @@ export default function TabLayout() {
           ios: {
             // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
+            backgroundColor: 'blue',
           },
-          default: {},
+          default: {
+            backgroundColor: 'blue',
+          },
         }),
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Início',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Pesquisar',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="search" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="publications"
+        options={{
+          title: 'Publicações',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heart-o" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="trades"
+        options={{
+          title: 'Trocas',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="exchange" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="data"
+        options={{
+          title: 'Meus dados',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user-circle-o" color={color} />,
         }}
       />
     </Tabs>
